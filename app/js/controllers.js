39fb70fbf7d2ef21cce918f20f78af47348df97a
Walter Pinson <walter@walterpinson.com>
2013-06-17 14:55:40 -0400

    Integrated git into WebStorm.  Made initial deletes and mods per pluralsight.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,11 +1,1 @@
 'use strict';
-
-/* Controllers */
-
-angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
-
-  }])
-  .controller('MyCtrl2', [function() {
-
-  }]);
--- app/js/filters.js
+++ app/js/filters.js
@@ -1,10 +1,2 @@
 'use strict';
 
-/* Filters */
-
-angular.module('myApp.filters', []).
-  filter('interpolate', ['version', function(version) {
-    return function(text) {
-      return String(text).replace(/\%VERSION\%/mg, version);
-    }
-  }]);
